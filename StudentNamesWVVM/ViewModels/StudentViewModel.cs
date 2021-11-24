1466382e40847fe81848027f2b5f5e513c3514db
IEUser <IEUser@MSEDGEWIN10>
2021-11-24 03:59:58 -0800

    Add project files.

---
+++ StudentNamesWVVM/ViewModels/StudentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using StudentNamesWVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentNamesWVVM.ViewModels
+{
+    internal class StudentViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Yuri", LastName = "Vosk" });
+            students.Add(new Student { FirstName = "Guy", LastName = "Tagger" });
+            students.Add(new Student { FirstName = "Ziv", LastName = "Brauner" });
+            students.Add(new Student { FirstName = "Taly", LastName = "Golan" });
+
+            Students = students;
+        }
+    }
+}
